b880d7588fb9d7be6779853bdb724c8265104157
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2020-12-22 20:32:27 -0500

    Update webapp name service

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://pythonwebapp.azurewebsites.net:$PORT/predict 
+     -X POST https://pythonwebapp-service.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
