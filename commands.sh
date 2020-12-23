68e988d78d61894fec50fc4667b5d9bee6857240
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2020-12-23 02:22:41 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,10 @@
-python3 -m venv ~/.udacity-project2 && \
+git clone git@github.com:JZerman2018/azure-pipeline-project2.git \
+ssh-keygen -m PEM -t rsa -b 4096 \
+cd azure-pipeline-project2 \
+make setup \
 source ~/.udacity-project2/bin/activate \
+make all \
 az webapp up -n pythonwebapp-service \
+./make_predict_azure_app.sh \
+chmod +x make_predict_azure_app.sh \
 az webapp log tail
